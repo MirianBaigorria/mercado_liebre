2e68e179d4d1f45f1a78f3b6feb3fe11f417c0d5
MirianBaigorria <146890174+MirianBaigorria@users.noreply.github.com>
2023-11-13 00:12:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-const port= porcess.env.PORT || 3000;
+const port= process.env.PORT || 3000;
 
 
 app.listen(port, ()=>{
